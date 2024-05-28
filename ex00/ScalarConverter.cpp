ac6dabd0204b0a12d3a644611c2f1478b22e15cc
Tom Lasseret <tlassere@k1r1p3.42mulhouse.fr>
2024-05-28 13:13:38 +0200

    Feat: formatage

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: tlassere <tlassere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 20:23:06 by tlassere          #+#    #+#             */
-/*   Updated: 2024/05/27 20:52:17 by tlassere         ###   ########.fr       */
+/*   Updated: 2024/05/28 13:13:01 by tlassere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,21 +32,64 @@ ScalarConverter& ScalarConverter::operator=( ScalarConverter const& )
 	return (*this);
 }
 
-long double	ScalarConverter::getResult(std::string const& str,
+static long double	getResult(std::string const& str,
 	t_scalarData *data)
 {
 	long double	result;
 
-	result = 0;
+	result = 0.5656565656;
 	(void)str;
+	(void)data;
 	return (result);
+}
+
+static void	set_type( std::string *type)
+{
+	type[0] = "char";
+	type[1] = "int";
+	type[2] = "float";
+	type[3] = "double";
+}
+
+static void	print_format(long double res, int i)
+{
+	if ((res - static_cast<long int>(res)) == 0)
+		std::cout <<  std::setprecision(1) << std::fixed;
+	
+	switch (i)
+	{
+		case 0:
+			std::cout << "'" << static_cast<char>(res) << "'";
+			break;
+		case 1:
+			std::cout << static_cast<int>(res);
+			break;
+		case 2:
+			std::cout << static_cast<float>(res) << 'f';
+			break;
+		case 3:
+			std::cout << static_cast<double>(res);
+			break;
+		default:
+			break;
+	}
 }
 
 void	ScalarConverter::convert(std::string const& str)
 {
-	std::string	data[SCALAR_SIZE];
-	long double	res;
+	t_scalarData	data[SCALAR_SIZE];
+	std::string		type[SCALAR_SIZE];
 
-	
-	(void)str;
+	long double	res;
+	set_type(type);
+	res = getResult(str, data);
+	for (int i = 0; i < SCALAR_SIZE; i++)
+	{
+		std::cout << type[i] << ": ";
+		if (data[i].empty())
+			print_format(res, i);
+		else
+			std::cout << data[i];
+		std::cout << "\n";
+	}
 }
--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: tlassere <tlassere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 19:59:23 by tlassere          #+#    #+#             */
-/*   Updated: 2024/05/27 20:50:02 by tlassere         ###   ########.fr       */
+/*   Updated: 2024/05/28 13:03:39 by tlassere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include <string>
 # include <iostream>
+# include <iomanip>
 
 # define SCALAR_SIZE 4
 
@@ -27,7 +28,6 @@ class	ScalarConverter
 		~ScalarConverter( void );
 		ScalarConverter( ScalarConverter const& );
 		ScalarConverter& operator= ( ScalarConverter const& );
-		long double	getResult(std::string const& str, t_scalarData *data);
 	
 	public:
 		static void	convert(std::string const& str);
