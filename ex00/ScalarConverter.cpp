61cd5e8fd0fe0b2690bda45a48637415ab9d8093
Tom Lasseret <tlassere@k1r1p3.42mulhouse.fr>
2024-05-28 14:00:21 +0200

    Feat: formatage

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: tlassere <tlassere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 20:23:06 by tlassere          #+#    #+#             */
-/*   Updated: 2024/05/28 13:13:01 by tlassere         ###   ########.fr       */
+/*   Updated: 2024/05/28 13:58:59 by tlassere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,14 +32,31 @@ ScalarConverter& ScalarConverter::operator=( ScalarConverter const& )
 	return (*this);
 }
 
+static void	checkerValue(long double res, t_scalarData *data)
+{
+	if (res < 32 || res > 126)
+		data[0] = "Non displayable";
+	// res != res to check is NaN
+	// res == (res -1) to check is infinity;
+	if (res != res || res == (res - 1))
+	{
+		data[0] = "impossible";
+		data[1] = "impossible";
+	}
+	if (res > 2147483647 || res < -2147483648)
+		data[1] = "Non displayable";
+	std::cout << res << std::endl;
+}
+
 static long double	getResult(std::string const& str,
 	t_scalarData *data)
 {
 	long double	result;
 
-	result = 0.5656565656;
-	(void)str;
-	(void)data;
+	result = std::strtod(str.c_str(), NULL);
+	if (result == HUGE_VAL)
+		std::cout << "hmm oky" << std::endl;
+	checkerValue(result, data);
 	return (result);
 }
 
@@ -53,9 +70,13 @@ static void	set_type( std::string *type)
 
 static void	print_format(long double res, int i)
 {
+	bool	plus_print;
+
+	plus_print = false;
 	if ((res - static_cast<long int>(res)) == 0)
 		std::cout <<  std::setprecision(1) << std::fixed;
-	
+	if (res == (res - 1) && res > 0)
+		plus_print = true;
 	switch (i)
 	{
 		case 0:
@@ -65,10 +86,12 @@ static void	print_format(long double res, int i)
 			std::cout << static_cast<int>(res);
 			break;
 		case 2:
-			std::cout << static_cast<float>(res) << 'f';
+			std::cout << ((plus_print) ? '+' : '\0')
+				<< static_cast<float>(res) << 'f';
 			break;
 		case 3:
-			std::cout << static_cast<double>(res);
+			std::cout << ((plus_print) ? '+' : '\0')
+				<< static_cast<double>(res);
 			break;
 		default:
 			break;
--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: tlassere <tlassere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 19:59:23 by tlassere          #+#    #+#             */
-/*   Updated: 2024/05/28 13:03:39 by tlassere         ###   ########.fr       */
+/*   Updated: 2024/05/28 13:58:33 by tlassere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 # include <string>
 # include <iostream>
 # include <iomanip>
+# include <cstdlib>
+# include <cmath>
 
 # define SCALAR_SIZE 4
 
