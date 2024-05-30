189ad41936c0a7d13606f3e23026207bc7a701c4
Tom Lasseret <tlassere@k1r1p2.42mulhouse.fr>
2024-05-30 13:54:33 +0200

    Feat: uiintptr

--- ex01/Serializer.hpp
+++ ex01/Serializer.hpp
@@ -6,12 +6,14 @@
 /*   By: tlassere <tlassere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 13:27:06 by tlassere          #+#    #+#             */
-/*   Updated: 2024/05/30 13:30:28 by tlassere         ###   ########.fr       */
+/*   Updated: 2024/05/30 13:35:41 by tlassere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SERIALIZER_HPP
 # define SERIALIZER_HPP
+
+typedef	unsigned long uintptr_t;
 
 class	Serializer
 {
